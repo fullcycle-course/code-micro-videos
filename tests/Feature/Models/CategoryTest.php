c14334a078ab772a20adbd2795d2342fb8e7903e
Andre Santos <andre@dexdev.com.br>
2020-06-25 19:43:46 -0300

    test: feature CategoryTest

--- tests/Feature/Models/CategoryTest.php
+++ tests/Feature/Models/CategoryTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature\Models;
 use App\Models\Category;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Tests\TestCase;
+use Ramsey\Uuid\Uuid as RUuid;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
@@ -33,4 +34,71 @@ class CategoryTest extends TestCase
             'deleted_at',
         ], $categoryKeys);
     }
+
+    public function testCreate()
+    {
+        $category = Category::create([
+            'name' => 'test1',
+        ]);
+        $category->refresh();
+
+        $this->assertEquals('test1', $category->name);
+        $this->assertNull($category->description);
+        $this->assertTrue($category->is_active);
+
+        $category = Category::create([
+            'name'        => 'test1',
+            'description' => null,
+        ]);
+        $this->assertNull($category->description);
+
+        $category = Category::create([
+            'name'      => 'test1',
+            'is_active' => false,
+        ]);
+
+        $this->assertFalse($category->is_active);
+
+        $category = Category::create([
+            'name'        => 'test1',
+            'description' => 'test_description',
+        ]);
+        $this->assertEquals('test_description', $category->description);
+
+        $category = Category::create([
+            'name'      => 'test1',
+            'is_active' => true,
+        ]);
+
+        $this->assertTrue($category->is_active);
+
+
+        $category = Category::create([
+            'name' => 'test1',
+        ]);
+        $this->assertIsString($category->id);
+        $this->assertTrue(RUuid::isValid($category->id));
+    }
+
+    public function testUpdate()
+    {
+        /** @var Category $category */
+        $category = factory(Category::class)->create([
+            'description' => 'test_description',
+            'is_active'=> false,
+        ])->first();
+
+        $data = [
+            'name'        => 'test_name_updated',
+            'description' => 'test_description_updated',
+            'is_active'   => true,
+        ];
+
+        $category->update($data);
+
+        foreach($data as $key => $value) {
+            $this->assertEquals($value, $category->{$key});
+        }
+    }
+
 }
