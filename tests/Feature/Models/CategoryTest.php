422b16106dbfc71b5cb05df19d00c1430c4bdd1a
Andre Santos <andre@dexdev.com.br>
2020-06-25 17:31:22 -0300

    test: feature CategoryTest

---
+++ tests/Feature/Models/CategoryTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Feature\Models;
+
+use App\Models\Category;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class CategoryTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testList()
+    {
+        factory(Category::class, 1)->create();
+        $categories = Category::all();
+        $this->assertCount(1, $categories);
+        $categoryKeys = array_keys($categories->first()->getAttributes());
+        $this->assertEqualsCanonicalizing([
+            'id',
+            'name',
+            'description',
+            'is_active',
+            'created_at',
+            'updated_at',
+            'deleted_at',
+        ], $categoryKeys);
+    }
+}
