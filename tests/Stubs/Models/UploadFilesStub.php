2c00a20e80b48c6b134aa3d11fd3dd2b71f6a505
Andre Santos <andre@dexdev.com.br>
2020-07-21 02:38:51 -0300

    test: upload files

---
+++ tests/Stubs/Models/UploadFilesStub.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tests\Stubs\Models;
+
+use App\Models\Traits\UploadFiles;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Schema\Blueprint;
+
+class UploadFilesStub extends Model
+{
+    use UploadFiles;
+//    protected $table    = 'upload_file_stubs';
+//    protected $fillable = ['name', 'file1', 'file2'];
+    public static $fileFields = ['file1', 'file2'];
+
+//
+//    public static function makeTable()
+//    {
+//        \Schema::create('upload_file_stubs', function (Blueprint $table) {
+//            $table->bigIncrements('id');
+//            $table->string('name');
+//            $table->string('file1');
+//            $table->string('file2');
+//            $table->timestamps();
+//        });
+//    }
+//
+//    public static function dropTable()
+//    {
+//        \Schema::dropIfExists('upload_file_stubs');
+//    }
+
+    public function uploadDir()
+    {
+        return '1';
+    }
+
+
+}
