f2599695618a3f854af44c16aef564289c67e340
André Santos <andre@dexdev.com.br>
2020-08-06 22:38:21 -0300

    feat: cast member resource api

--- app/Http/Controllers/Api/CastMemberController.php
+++ app/Http/Controllers/Api/CastMemberController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Http\Resources\CastMemberResource;
 use App\Models\CastMember;
 
 class CastMemberController extends BasicCrudController
@@ -30,5 +31,16 @@ class CastMemberController extends BasicCrudController
     {
         return $this->rules;
     }
+
+    protected function resourceCollection()
+    {
+        return $this->resource();
+    }
+
+    protected function resource()
+    {
+        return CastMemberResource::class;
+    }
+
 
 }
